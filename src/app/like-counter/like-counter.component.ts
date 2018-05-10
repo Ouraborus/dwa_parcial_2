0d06db21313d56c2df681ede0e5c7f97407b2818
Ouraborus <1222eca@gmail.com>
2018-05-10 10:17:31 -0500

    Adding searchbar to the grid

--- src/app/like-counter/like-counter.component.ts
+++ src/app/like-counter/like-counter.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./like-counter.component.scss']
 })
 export class LikeCounterComponent implements OnInit {
+  public rating:number;
 
   constructor() { }
 
   ngOnInit() {
+    this.rating = 0;
+  }
+  rateComic(bool) {
+    if(bool){
+      this.rating++;
+    } else {
+      this.rating--;
+    }
   }
 
 }
